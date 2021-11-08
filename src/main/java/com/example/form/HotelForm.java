1c6c98fd8368b5da69921dca3e36388fc2b87fea
kenji-otomo <kenji.otomo@rakus-partners.co.jp>
2021-11-08 17:25:18 +0900

    Hotelドメインとフォームを作成

--- src/main/java/com/example/form/HotelForm.java
+++ src/main/java/com/example/form/HotelForm.java
@@ -2,4 +2,19 @@ package com.example.form;
 
 public class HotelForm {
 
+	private Integer price;
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "HotelForm [price=" + price + "]";
+	}
+	
 }
