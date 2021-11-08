da0933dcb4001d17e7dbdd57b2bfdb5d80c1d094
kenji-otomo <kenji.otomo@rakus-partners.co.jp>
2021-11-08 15:32:47 +0900

    indexメソッド作成

--- src/main/java/com/example/controller/IntermediateController.java
+++ src/main/java/com/example/controller/IntermediateController.java
@@ -1,5 +1,22 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.service.IntermediateService;
+
+@Controller
+@RequestMapping("/intermadiate")
 public class IntermediateController {
 
+	@Autowired
+	IntermediateService service;
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
 }
