6771e2a8a07371451b489a307d55c4583b39f46f
kenji-otomo <kenji.otomo@rakus-partners.co.jp>
2021-11-08 17:37:00 +0900

    サービス作成

--- src/main/java/com/example/service/HotelService.java
+++ src/main/java/com/example/service/HotelService.java
@@ -1,5 +1,24 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Hotel;
+import com.example.repository.HotelRepository;
+
+@Service
+@Transactional
 public class HotelService {
+	
+	@Autowired
+	HotelRepository repository;
+	
+	public List<Hotel> searchByLessThanPrice(Integer price) {
+		
+		return repository.searchByLessThanPrice(price);
+	}
 
 }
