fcb53a4bd2eec18fcabc81bd0dd87bee6e3878d4
kenji-otomo <kenji.otomo@rakus-partners.co.jp>
2021-11-08 17:58:50 +0900

    コントローラーとホテルサーチhtmlを作成

--- src/main/java/com/example/controller/HotelController.java
+++ src/main/java/com/example/controller/HotelController.java
@@ -1,5 +1,42 @@
 package com.example.controller;
 
-public class HotelController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Hotel;
+import com.example.form.HotelForm;
+import com.example.service.HotelService;
+
+@Controller
+@RequestMapping("/hotel")
+public class HotelController {
+	
+	@Autowired
+	HotelService service;
+	
+	@ModelAttribute
+	public HotelForm setyUpHotelForm() {
+		return new HotelForm();
+	}
+	
+	@RequestMapping("")
+	public String index() {
+		
+		return "hotelsearch";
+	}
+
+	@RequestMapping("/search")
+	public String search(Model model,HotelForm form) {
+		
+		List<Hotel>hotelList = service.searchByLessThanPrice(form.getPrice());
+		
+		model.addAttribute("hotelList", hotelList);
+		
+		return "hotelsearch";
+	}
 }
