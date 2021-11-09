e4064464215a5408ef960bf8d94fbe22fc3e0f57
kenji-otomo <kenji.otomo@rakus-partners.co.jp>
2021-11-09 12:11:11 +0900

    サービスに検索するメソッド、コントローラーにindex,findを作成

--- src/main/java/com/example/controller/ClothController.java
+++ src/main/java/com/example/controller/ClothController.java
@@ -1,5 +1,39 @@
 package com.example.controller;
 
-public class ClothController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Cloth;
+import com.example.form.ClothForm;
+import com.example.service.ClothService;
+
+@Controller
+@RequestMapping("/cloth")
+public class ClothController {
+	
+	public ClothForm setUpClothForm() {
+		return new ClothForm();
+	}
+
+	@Autowired
+	ClothService service;
+	
+	@RequestMapping("")
+	public String index() {
+		return "clothes";
+	}
+	
+	@RequestMapping("/find")
+	public String find(ClothForm form,Model model) {
+		
+		List<Cloth>list = service.find(form);
+		
+		model.addAttribute("resultList", list);
+		
+		return index();
+	}
 }
--- src/main/java/com/example/service/ClothService.java
+++ src/main/java/com/example/service/ClothService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.Cloth;
+import com.example.form.ClothForm;
 import com.example.repository.ClothRepository;
 
 @Service
@@ -16,7 +18,26 @@ public class ClothService {
 	@Autowired
 	ClothRepository repository;
 	
-	public List<Cloth> find(Integer gender,String color) {
-		return repository.findByGenderAndColor(gender, color);
+	public List<Cloth> find(ClothForm form) {
+		
+		List<String>colorList = new ArrayList<>();
+		for (Integer colorCode : form.getColorList()) {
+			switch(colorCode) {
+			case 1:
+				colorList.add("赤");
+				break;
+			case 2:
+				colorList.add("青");
+				break;
+			case 3:
+				colorList.add("白");
+				break;
+			case 4:
+				colorList.add("黄");
+				break;
+			}
+		}
+		
+		return repository.findByGenderAndColor(form.getGenderList().get(0),colorList.get(0));
 	}
 }
